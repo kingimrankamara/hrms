7b548aeac1098644155fdf54e5173be6dc9b5dc2
medvik <kabiamohamed1997@gmail.com>
2023-01-12 00:55:27 +0000

    reset password by  email using staffId

--- api/routes/auth/staffForgetPassword.js
+++ api/routes/auth/staffForgetPassword.js
@@ -15,21 +15,22 @@ router
   .post(allowCors.corsWithOptions, async (req, res) => {
   try{
   const emailSchem =joi.object({
-      email:joi.string().required().email()
+      staffId:joi.string()
   });
   const {error} = emailSchem.validate(req.body)
   if (error) res.status(401).json({error:error, message:'invalid email'})
 
   //chek if user exist
-  const user = await Staff.findOne({email:req.body.email})
+  const user = await Staff.findOne({staffId:req.body.staffId})
   if (!user) return res.status(401).json({error:error, message:'Invalid email'})
   //generat verifecation code
   const resetCode = Math.floor(1000 + Math.random() * 9000);
   let payloadData={
-      to:req.body.email,
+      to:user.email,
       subject:"Reset Password",
       content:"Hi we received a request to reset your password, please use this code to reset your password "+resetCode
   }
+  console.log(payloadData)
   mailSender(payloadData);//nodemailer middleware
   
    user.resetPassword = {
@@ -41,7 +42,7 @@ router
       console.log('unable to send email')
       res.status(400).send(err);
   })
-  res.send('resetCode');
+  res.status(200).json(user.email);
 }catch(err){
   console.log(err);
 }
